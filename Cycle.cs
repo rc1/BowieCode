a738a0abf406e74e624e1a76973dbdc707a93242
Ross Cairns <ross@electricglen.com>
2016-09-21 11:03:29 +0100

    Added Cycle

---
+++ Cycle.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace BowieCode  {
+	
+	public class Cycle<T> {
+
+		public List<T> enumerable {
+			get { return _enumerable; }
+		}
+
+		private List<T> _enumerable;
+		private int _idx = -1;
+
+		public Cycle ( List<T> enumerable ) {
+			_enumerable = enumerable;
+		}
+
+		public T Next () {
+			if (++_idx >= enumerable.Count) {
+				_idx = 0;
+			}
+			return _enumerable[_idx];
+		}
+
+		public T Previous () {
+			if ( --_idx < 0 ) {
+				_idx = _enumerable.Count - 1;
+			}
+			return _enumerable[_idx];
+		}
+	}
+
+}
