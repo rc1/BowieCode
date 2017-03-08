8d382a8f6f4882ec83527c8c1c1c52a9c83e1222
Ross Cairns <ross@electricglen.com>
2017-03-08 22:16:42 +0000

    Added DistrbuteChildren

---
+++ Components/DistributeChildren.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace BowieCode {
+
+	/// <summary>
+	/// Distributes it's children between two points. The two points can be manipulated in the scene view.
+	/// </summary>
+	public class DistributeChildren : MonoBehaviour {
+
+		public Vector3 a = new Vector3( -1.0f, 0.0f, 0.0f );
+		public Vector3 b = new Vector3( 1.0f, 0.0f, 0.0f );
+
+#if UNITY_EDITOR
+		[InspectorButton(MethodName="RedistributeChildren",ButtonText="Distribute Children")]
+		public InspectorButton distributeButton;
+#endif
+
+		/// <summary>
+		/// Preform the distribution of the children.
+		/// </summary>
+		public void RedistributeChildren () {
+			for ( float i = 0.0f; i < transform.childCount; i++ ) {
+				Vector3 pos = Vector3.Lerp( a + transform.position, b + transform.position, i / ( transform.childCount - 1.0f ) );
+				transform.GetChild( (int)i ).position = pos;
+			}
+		}
+
+		/// <summary>
+		/// Draw a line with the points
+		/// </summary>
+		void OnDrawGizmosSelected () {
+			Gizmos.color = Color.cyan;
+			Gizmos.DrawLine( a + transform.position, b + transform.position );
+			Gizmos.color = Color.yellow;
+			for ( float i = 0.0f; i < transform.childCount; i++ ) {
+				Vector3 pos = Vector3.Lerp( a, b, i / ( transform.childCount - 1.0f ) );
+				Gizmos.DrawSphere( transform.position + pos, 0.1f );
+			}
+		}
+			
+	}
+
+}
---
+++ Components/Editor/DistributeChildrenEditor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace BowieCode {
+
+	[CustomEditor(typeof(DistributeChildren))]
+	public class DistributeChildrenEditor : Editor {
+		
+		void OnSceneGUI () {
+
+			var distributeChildren = target as DistributeChildren;
+
+			Vector3 oldA = distributeChildren.a;
+			Vector3 oldB = distributeChildren.b;
+
+			Vector3 newA = Handles.PositionHandle( oldA + distributeChildren.transform.position, Quaternion.Euler( distributeChildren.transform.up ) ) - distributeChildren.transform.position;
+			Vector3 newB = Handles.PositionHandle( oldB + distributeChildren.transform.position, Quaternion.Euler( distributeChildren.transform.up ) ) - distributeChildren.transform.position;
+
+			if ( oldA != newA || newB != oldB ) {
+				distributeChildren.a = newA;
+				distributeChildren.b = newB;
+				distributeChildren.RedistributeChildren();
+			}
+
+		}
+
+	}
+
+}
