e96179091ca2e289019b5b32f9a6ba6dae6208cd
Ross Cairns <ross@electricglen.com>
2017-03-16 21:25:36 +0000

    Added GetLastKeyTime

--- Utils/BowieExtensions.cs
+++ Utils/BowieExtensions.cs
@@ -1,13 +1,29 @@
 ﻿using System.Collections.Generic;
 using System;
+using UnityEngine;
 
 namespace BowieCode {
 
 	public static class BowieExtensions {
 
+		/// <summary>
+		/// Collection iterator with the value and index
+		/// </summary>
 		public static void ForEachWithIndex<T> ( this IEnumerable<T> ie, Action<T, int> action ) {
 			var i = 0;
 			foreach ( var e in ie ) action( e, i++ );
+		}
+
+		/// <summary>
+		/// Gets the last key time.
+		/// </summary>
+		/// <returns>The last key's time.</returns>
+		/// <param name="curve">Curve.</param>
+		public static float GetLastKeyTime ( this AnimationCurve curve ) {
+			if ( curve.length == 0 ) {
+				return 0.0f;
+			}
+			return curve[ curve.length - 1 ].time;
 		}
 
 	}
