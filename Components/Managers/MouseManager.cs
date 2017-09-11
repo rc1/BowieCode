1e6bd43736f5a6ac0bf0d4158e69472271125d2c
Ross Cairns <ross@electricglen.com>
2017-09-11 15:41:44 +0100

    Added events to show and hide mouse

--- Components/Managers/MouseManager.cs
+++ Components/Managers/MouseManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using BowieCode;
@@ -14,18 +15,32 @@ public class MouseManager : MonoBehaviour {
     private Vector3 _lastMousePosition;
     private float _lastMouseMoveTime = 0;
 
+    public event Action OnMouseShow;
+    public event Action OnMouseHide;
+    
+    private enum MouseState {
+        NotSet,
+        Visible,
+        Invisible
+    }
+    private MouseState _lastMouseState = MouseState.NotSet;
 
     void Awake () {
         DefaultInstance<MouseManager>.Set( this );
-        
+    }
+
+    void Start() {
         // Hide the mouse if it does not exist
         if ( !Input.mousePresent ) {
-            Cursor.visible = false;
+            SetMouseVisibleState( false );
         }
-
     }
 
     void Update () {
+
+        if ( !Input.mousePresent ) {
+            return;
+        }
         
         if ( !BowieMath.CompareVectors( _lastMousePosition, Input.mousePosition ) ) {
             _lastMouseMoveTime = Time.fixedTime;
@@ -33,8 +48,28 @@ public class MouseManager : MonoBehaviour {
 
         _lastMousePosition = Input.mousePosition;
 
-        Cursor.visible = (Time.fixedTime - _lastMouseMoveTime < afterSeconds);
+        SetMouseVisibleState( Time.fixedTime - _lastMouseMoveTime < afterSeconds );
 
+    }
+
+    private void SetMouseVisibleState ( bool shouldBeVisible ) {
+
+        var newMouseState = shouldBeVisible ? MouseState.Visible : MouseState.Invisible;
+
+        Cursor.visible = shouldBeVisible;
+       
+        // Raise the event if there were a change
+        if ( newMouseState != _lastMouseState ) {
+            if ( shouldBeVisible && OnMouseShow != null ) {
+                OnMouseShow();
+            }
+            else if ( !shouldBeVisible && OnMouseHide != null ) {
+                OnMouseHide();
+            }
+        }
+        
+        _lastMouseState = newMouseState;
+        
     }
 
 }
